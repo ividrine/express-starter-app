500c99680ea379be3ec7d94193942f93590caf44
Isaac Vidrine <idvidrine@gmail.com>
2026-01-19 04:47:38 -0500

    fix: lower coverage threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -22,10 +22,10 @@ export default defineConfig({
     sequence: { concurrent: false },
     coverage: {
       thresholds: {
-        lines: 100,
-        functions: 100,
-        branches: 100,
-        statements: 100
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80
       },
       provider: "istanbul",
       reporter: ["text", "lcov", "cobertura", "html"],
